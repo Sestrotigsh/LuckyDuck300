53b72463ffe54e8fba53acf0cc80c7b612b8e6c2
Dracico <szymanski.jim@icloud.com>
2018-09-02 21:30:13 +1000

    noop

--- Fight for the Night/Assets/Scripts/EnemyWaves.cs
+++ Fight for the Night/Assets/Scripts/EnemyWaves.cs
@@ -4,49 +4,97 @@ using UnityEngine;
 using UnityEngine.Networking;
 
 public class EnemyWaves : NetworkBehaviour {
-// SPAWN GENERIC IN WAVES TO ATTACK THE PLAYER AND SHOW ON OTHER PLAYERS GAME
+    // SPAWN GENERIC IN WAVES TO ATTACK THE PLAYER AND SHOW ON OTHER PLAYERS GAME
 
-	// Spawners and enemy in the wave
-	public GameObject playerSpawner;
+    // Spawners and enemy in the wave
+
+    private GameObject spawner;
 	//public GameObject enemy;
 	public GameObject alienEnemy;
 	public GameObject slasherEnemy;
-
-    private GameObject alienPrivate;
-    private GameObject slasherPrivate;
 
 	private int nextSpawn; // Determine the start of the next Wave
 	public int initialSpawn; // Determine the time for the first wavespawn
 	private int spawnDistance = 10; // Determine the time distance between the end and the start of the next wave 
 	public int waveLength = 3; // Number of second / number of minions the wave will be
 	private int remainingMinions; // The current time the wave start + the waveLength
-	private PlayerNetwork PlayerNet; // networking attached to the player
+
+
+    // Other variables
+    private bool player0Check = false;
+    private bool player1Check = false;
+
+    private string player0Opp = null;
+    private string player1Opp = null;
+
+    private PlayerNetwork player0;
+    private PlayerNetwork player1;
 
 	// Use this for initialization
 	void Start () {
-		// only spawn for the local player
-		PlayerNet = this.GetComponent<PlayerNetwork> ();
-		if (!PlayerNet.local) {
-			enabled = false;
-		}
-       
+        // only spawn for the local player
+
+        spawner = GameObject.FindGameObjectWithTag("Spawn");
+
 		remainingMinions = waveLength;
 		nextSpawn = initialSpawn;
-		playerSpawner = GameObject.FindGameObjectWithTag ("Spawn" + PlayerNet.team);
-
-       
-        
-        
-		//if (transform.Find("AlienClothes").gameObject.activeSelf == true) {
-				//enemy = alienEnemy;
-		//} else if (transform.Find("SlasherClothes").gameObject.activeSelf == true) {
-				//enemy = slasherEnemy;
-		//}
+	    
+        if (this.tag != "Player0")
+        {
+            enabled = false;
+        }
 	}
 
 	// Update is called once per frame
 	void Update () {
-		Wave();
+
+        // Check the first player is in game
+        if (player0Check == false)
+        {
+            if ( GameObject.FindGameObjectWithTag("Player0"))
+            {
+                player0 = GameObject.FindGameObjectWithTag("Player0").GetComponent<PlayerNetwork>();
+                player0Check = true;
+            }
+        }
+
+        // Check the second player is in game
+        if (player1Check == false)
+        {
+            if (GameObject.FindGameObjectWithTag("Player1"))
+            {
+                player1 = GameObject.FindGameObjectWithTag("Player1").GetComponent<PlayerNetwork>();
+                player1Check = true;
+            }
+        }
+
+        if (player0Check == true && player1Check == true && (player0Opp == null || player1Opp == null))
+        {
+            if (player0.opponent.transform.Find("AlienClothes").gameObject.activeSelf == true)
+            {
+                player0Opp = "Alien";
+            }
+            else
+            {
+                player0Opp = "Slasher";
+            }
+
+            if (player1.opponent.transform.Find("AlienClothes").gameObject.activeSelf == true)
+            {
+                player1Opp = "Alien";
+            } else
+            {
+                player1Opp = "Slasher";
+            }
+
+        }
+            
+
+        if (player0Opp != null && player1Opp != null)
+        {
+            Wave();
+        }
+		
 	}
 
 	/// <summary>
@@ -56,23 +104,23 @@ public class EnemyWaves : NetworkBehaviour {
 		if (Time.timeSinceLevelLoad > nextSpawn) {
 			if (remainingMinions > 0) {
 				nextSpawn = nextSpawn + 1;
-				if (PlayerNet.opponent != null) {
-                    if (PlayerNet.opponent.transform.Find("AlienClothes").gameObject.activeSelf == true) {
-                        CmdSpawnEnemyAlien(playerSpawner.transform.position);
-                        
-                    } else if (PlayerNet.opponent.transform.Find("SlasherClothes").gameObject.activeSelf == true) {
-						CmdSpawnEnemySlasher(playerSpawner.transform.position);
-                        
-                    }
-				} else {
-					if (transform.Find("AlienClothes").gameObject.activeSelf == true) {
-                        CmdSpawnEnemyAlien(playerSpawner.transform.position);
-                       
-                    } else {
-                        CmdSpawnEnemySlasher(playerSpawner.transform.position);
-                        
-                    }
-				}
+				
+                if (player0Opp == "Alien")
+                {
+                    CmdSpawnEnemyAlien( 0);
+                } else
+                {
+                    CmdSpawnEnemySlasher( 0);
+                }
+
+                if (player1Opp == "Alien")
+                {
+                    CmdSpawnEnemyAlien( 1);
+                } else
+                {
+                    CmdSpawnEnemySlasher(1);
+                }
+
 				remainingMinions = remainingMinions - 1;
 			} else {
 				remainingMinions = waveLength;
@@ -86,16 +134,17 @@ public class EnemyWaves : NetworkBehaviour {
 	/// </summary>
 	/// <param name="pos">the position to spawn in</param>
 	[Command]
-	void CmdSpawnEnemyAlien(Vector3 pos) {
-		var currentEnemy = Instantiate(alienEnemy, pos, Quaternion.Euler(0, 0, 0)) as GameObject;
-        currentEnemy.tag = "Enemy" + PlayerNet.team;
+	void CmdSpawnEnemyAlien( int minionTeam) {
+		var currentEnemy = Instantiate(alienEnemy, spawner.transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
+        currentEnemy.tag = "Enemy" + minionTeam;
         NetworkServer.Spawn (currentEnemy);
         
     }
 
 	[Command]
-	void CmdSpawnEnemySlasher(Vector3 pos) {
-		var currentEnemy = Instantiate(slasherEnemy, pos, Quaternion.Euler(0, 0, 0)) as GameObject;
+	void CmdSpawnEnemySlasher(int minionTeam) {
+		var currentEnemy = Instantiate(slasherEnemy, spawner.transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
+        currentEnemy.tag = "Enemy" + minionTeam;
         NetworkServer.Spawn (currentEnemy);
 	}
 
