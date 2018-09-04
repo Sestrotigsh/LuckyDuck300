d8fbd6344b7b3d89e6847938e161d356eb1d2bc0
mashwin1993 <31690715+mashwin1993@users.noreply.github.com>
2018-09-05 00:57:54 +1000

    Folder and Branch

---
+++ Fight for the Night/Assets/NewTowerUI/CameraRayCast.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRayCast : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void InteractRaycast()
+    {
+        Vector3 playerPosition = transform.position;
+        Vector3 forwardDirection = transform.forward;
+
+        Ray interactionRay = new Ray(playerPosition, forwardDirection);
+        RaycastHit interactionRayHit;
+        float interactionRayLength = 5.0f;
+
+        Vector3 interactionRayEndpoint = forwardDirection * interactionRayLength;
+        Debug.DrawLine(playerPosition, interactionRayEndpoint);
+    }
+
+}
